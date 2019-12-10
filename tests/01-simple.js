00750c1794fba5cd341e7e1208ce4ba783ca7966
marcelloantunes <marcello.ants@gmail.com>
2019-12-10 13:49:27 -0300

    create test framework

---
+++ tests/01-simple.js
@@ -0,0 +1,19 @@
+const sum = (a, b) => a + b
+const subtract = (a, b) => a - b
+
+// const sum = (a, b) => a - b
+// const subtract = (a, b) => a + b
+
+let result = sum(3, 7)
+let expected = 10
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+} else console.log("no error")
+
+result = subtract(7, 3)
+expected = 4
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+} else console.log("no error")
